e85c1aff39e90bf8d8ec46cce18c52d3cb6c2574
TrashGameDeveloper <trashgamedeveloper@gmail.com>
2022-10-12 21:29:23 -0400

    initial commit

---
+++ build/CMakeFiles/Blended.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Blended"
+  "Blended.pdb"
+  "CMakeFiles/Blended.dir/main.cpp.o"
+  "CMakeFiles/Blended.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blended.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Blended.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Blended.
+# This may be replaced when dependencies are built.
